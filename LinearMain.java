0b795d6a2fe88c6fa60302dbd687d5a495d59e6b
maclab2019 <maclab2019@CUS-MACLAB-4521.local>
2021-09-22 10:35:20 -0400

    Integer Slopes

--- LinearMain.java
+++ LinearMain.java
@@ -5,8 +5,10 @@ import main.LinearRegression;
 public class LinearMain {
 	public static void main(String[] args) {
 		double[] x = {1, 2, 3};
-		double[] y = {1, 4, 3};
+		double[] y = {4, 8, 12};
 		
 		LinearRegression rg = new LinearRegression(x, y); 
+		
+		System.out.println(rg.LR()); 
 	}
 }
--- LinearRegression.java
+++ LinearRegression.java
@@ -1,19 +1,63 @@
 package main;
 
 public class LinearRegression {
-	public LinearRegression(double[] x, double[] y) {
+	static double[] x, y;
+	public LinearRegression(double[] x_in, double[] y_in) {
+		x = x_in;
+		y = y_in; 
+	}
+	
+	public double LR() {
 		double slope_initial = 1;
 		double y_inter_initial = 0; 
-		double sum_distance = 0; 
 		
-		for (int i=0; i<x.length; i++) {
-			sum_distance += Math.pow((y[i]-find_point(slope_initial, y_inter_initial, x[i])), 2); 
+		boolean sign_change_flag = false;
+		double places = 1; 
+		double end_condition = 1; 
+		
+		while (end_condition < 5) {
+			double sum_distance = 0; 
+			if (sign_change_flag) {
+				places = places/10; 
+				sign_change_flag = false; 
+			}
+		
+			for (int i=0; i<x.length; i++) {
+				double compare_point = y[i] - find_point(slope_initial, y_inter_initial, x[i]); 
+				
+				sum_distance += (preserve_sign(compare_point))*Math.pow(compare_point, 2); 
+			}
+			
+			if (sum_distance == 0) {
+				return slope_initial; 
+			} else if (sum_distance > 0) {
+				slope_initial += places; 
+				System.out.print(sum_distance);
+				System.out.print(" added "); 
+				System.out.println(slope_initial); 
+			} else {
+				slope_initial -= places;
+				System.out.print(sum_distance);
+				System.out.print(" subtracted "); 
+				System.out.println(slope_initial); 
+			}
+			
+			end_condition++; 
 		}
-		
-		System.out.println(sum_distance);  
+		return slope_initial; 
 	}
+	
+	
 	
 	public static double find_point(double slope, double inter, double x) {
 		return slope*x+inter; 
+	}
+	
+	public static double preserve_sign(double point) {
+		double sign = 1; 
+		if (point != 0) {
+			sign = point/Math.abs(point); 
+		}
+		return sign;
 	}
 }
