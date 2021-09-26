bf2f205159c92e45847e2c06a2f755a101aa8922
BuildTools <unconfigured@null.spigotmc.org>
2021-09-26 13:18:01 -0400

    Lottery finished

--- Lottery.java
+++ Lottery.java
@@ -1,5 +1,21 @@
 package main;
+import java.util.Scanner;
 
 public class Lottery {
-	
+	public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in); 
+        System.out.println("Guess a number 0-99:");
+        int guess = keyboard.nextInt();
+        
+        int random = (int) (99*Math.random()); 
+
+
+        if (guess == random) {
+            System.out.println("Correct Guess!"); 
+        } else {
+            System.out.println(String.format("Wrong, it was %s", random)); 
+        }
+
+        keyboard.close(); 
+    }
 }
