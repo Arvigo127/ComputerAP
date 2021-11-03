712e5ce09972f7a2385aaf4a9c00d6bcb30c2b13
maclab2019 <maclab2019@CUS-MACLAB-4521.local>
2021-11-03 10:33:56 -0400

    count words to blackbox

--- BlackBox.java
+++ BlackBox.java
@@ -5,6 +5,7 @@ public class BlackBox {
 		char[][] charArray = {{'\0', 'h', 'i', '\0', 't', 'h', 'e', 'r', 'e'}, 
 							  {'m', 'y', '\0', 'f', 'r', 'i', 'e', 'n', 'd'}};
 			System.out.println(deObfuscate(charArray)); 
+			System.out.println("Number of words: " + countWords(charArray));
 		}; 
 		
 	
@@ -25,5 +26,26 @@ public class BlackBox {
 		}
 		
 		return resultString;
+	}
+	
+	public static int countWords(char[][] input) {
+		
+		int n_words = 0; 
+		String currentWord = "";
+		
+		for(int row = 0; row<input.length; row++) {
+			for(int column = 0; column<input[0].length; column++) {
+				if(input[row][column] > 0) {
+					currentWord = currentWord.concat(String.valueOf(input[row][column]));
+					
+					if(column == input[0].length-1 || input[row][column+1] == '\0') {
+						n_words++;
+						currentWord = "";
+					}
+				}
+			}
+		}
+		
+		return n_words; 
 	}
 }
