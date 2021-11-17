3bdad913d98e0b76c83de92dacc0159f94105879
maclab2019 <maclab2019@CUS-MACLAB-4521.local>
2021-11-17 10:35:58 -0500

    selection sort baby

--- LinkedList.java
+++ LinkedList.java
@@ -7,5 +7,7 @@ public class LinkedList {
 		myLinkedList = new Node[1]; 
 	}
 	
+
+	
 	
 }
---
+++ SelectionSort.java
@@ -0,0 +1,48 @@
+package main;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+	
+	public static int findIndexOfMin(int[] items, int start, int end) {
+		int minIndex = start; 
+		for(int i = start; i<items.length; i++) {
+			if(items[i]<items[minIndex]) {
+				minIndex = i; 
+			}
+		}
+		return minIndex; 
+	}
+	
+	public static int[] swapIndex(int[] input, int origin, int destination) {
+		int temp = input[destination];
+		input[destination] = input[origin]; 
+		input[origin] = temp; 
+		
+		return input; 
+		
+	}
+	
+	public static int[] sortArray(int[] input) {
+		
+		int start = 0; 
+		
+		for(int i = 0; i<input.length; i++) {
+			int minIndex = findIndexOfMin(input, start, input.length-1); 
+			
+			input = swapIndex(input, minIndex, i); 
+			
+			start = i+1; 
+		}
+		
+		return input; 
+	}
+	
+	public static void main(String[] args) {
+		int[] unsorted = {5, 6, 8, 5, 5, 1, -3, 2}; 
+		
+		int[] sorted = sortArray(unsorted); 
+		
+		System.out.println(Arrays.toString(sorted)); 
+	}
+}
